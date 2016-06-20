dc30144ac701671c9677149a7799b8d7b6a3776d
Jazzmin Vangeel <jazzvangeel@gmail.com>
2016-06-20 18:37:37 -0400

    on submit function sarted

---
+++ assets/js/lf-module.js
@@ -0,0 +1,11 @@
+// this is going to the module function
+// jQuery(document).ready(
+	function lfmodule() {
+	jQuery( '#module--form' ).submit(function( event ) {
+		event.preventDefault();
+		$(".module").removeClass("module--visible").addClass("module--notvisible");
+		// alert("on submit has triggered the on submit function!");
+	});
+}; // end of lffunction
+
+lfmodule();
