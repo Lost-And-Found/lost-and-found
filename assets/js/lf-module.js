93e076545a81a9850f6e12675de89a86e2819348
Jazzmin Vangeel <jazzvangeel@gmail.com>
2016-09-28 16:47:53 -0400

    further work on module styling and function

--- assets/js/lf-module.js
+++ assets/js/lf-module.js
@@ -5,6 +5,12 @@
 		event.preventDefault();
 		$(".module").removeClass("module--visible").addClass("module--notvisible");
 		// alert("on submit has triggered the on submit function!");
+		// $('#module--form').submit();
+	});
+	jQuery( '#nav-toggle' ).click(function( event ) {
+		// event.preventDefault();
+		$(".module").removeClass("module--visible").addClass("module--notvisible");
+		// alert("on submit has triggered the on submit function!");
 	});
 }; // end of lffunction
 
