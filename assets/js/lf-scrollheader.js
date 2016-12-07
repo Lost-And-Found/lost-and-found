3d978fa3931cade605e755b3963b86b964a8687b
Jazzmin Vangeel <jazzvangeel@gmail.com>
2016-12-06 23:02:13 -0500

    slide out nav started

--- assets/js/lf-scrollheader.js
+++ assets/js/lf-scrollheader.js
@@ -7,11 +7,15 @@ window.addEventListener('scroll', function(e){
          jQuery(".wrapper__nav").addClass("header_small");
          jQuery(".nav--dark").addClass("nav__small");
          jQuery(".logo").addClass("logo_small");
+         jQuery("#navcollectionlist").addClass("small_dropdown");
+         jQuery("#navbrandlist").addClass("small_dropdown");
     } else if (distanceY < growOn ) {
         jQuery("body").removeClass('small_heading');
         jQuery(".wrapper__nav").removeClass('header_small');
         jQuery(".nav--dark").removeClass('nav__small');
         jQuery(".logo").removeClass('logo_small');
+        jQuery("#navcollectionlist").removeClass("small_dropdown");
+        jQuery("#navbrandlist").removeClass("small_dropdown");
     } else {
         console.log("werking?");
     }
