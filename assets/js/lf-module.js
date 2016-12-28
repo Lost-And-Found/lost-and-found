439237f5b5c43e2f5b3109950058cf156564d7a4
Jazzmin Vangeel <jazzvangeel@gmail.com>
2016-12-27 19:06:25 -0500

    fin

--- assets/js/lf-module.js
+++ assets/js/lf-module.js
@@ -1,17 +1,17 @@
 // this is going to the module function
 // jQuery(document).ready(
-	function lfmodule() {
-	jQuery( '#module--form' ).submit(function( event ) {
-		event.preventDefault();
-		$(".module").removeClass("module--visible").addClass("module--notvisible");
-		// alert("on submit has triggered the on submit function!");
-		// $('#module--form').submit();
-	});
-	jQuery( '#nav-toggle' ).click(function( event ) {
-		// event.preventDefault();
-		$(".module").removeClass("module--visible").addClass("module--notvisible");
-		// alert("on submit has triggered the on submit function!");
-	});
-}; // end of lffunction
+// 	function lfmodule() {
+// 	jQuery( '#module--form' ).submit(function( event ) {
+// 		event.preventDefault();
+// 		$(".module").removeClass("module--visible").addClass("module--notvisible");
+// 		// alert("on submit has triggered the on submit function!");
+// 		// $('#module--form').submit();
+// 	});
+// 	jQuery( '#nav-toggle' ).click(function( event ) {
+// 		// event.preventDefault();
+// 		$(".module").removeClass("module--visible").addClass("module--notvisible");
+// 		// alert("on submit has triggered the on submit function!");
+// 	});
+// }; // end of lffunction
 
-lfmodule();
+// lfmodule();
